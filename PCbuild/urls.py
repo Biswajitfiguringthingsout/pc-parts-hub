11b5aee4fdedd25c1a893e4e096f17acf6cac19f
Biswajit <biswajitdas882066@gmail.com>
2026-06-20 01:26:40 +0530

    Implement homepage, search, sorting, product detail pages, and dark mode option

--- PCbuild/urls.py
+++ PCbuild/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('products/', include('products.urls')),
     
 ]
@@ -28,3 +30,4 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+   
--- products/views.py
+++ products/views.py
@@ -10,14 +10,29 @@ def product_list(request):
     if query:
         products = products.filter(name__icontains=query)
 
+    sort = request.GET.get('sort', '')
+
+    if sort == 'price_low':
+        products = products.order_by('price')
+
+    elif sort == 'price_high':
+        products = products.order_by('-price')
+
+    elif sort == 'name_asc':
+        products = products.order_by('name')
+
+    elif sort == 'name_desc':
+        products = products.order_by('-name')
+
     return render(
         request,
         'products/product_list.html',
         {
             'products': products,
             'query': query,
+            'sort': sort,
         }
-    )
+    ) 
 
 
 def product_detail(request, id):
@@ -38,11 +53,36 @@ def products_by_category(request, category):
     if query:
         products = products.filter(name__icontains=query)
 
+    sort = request.GET.get('sort', '')
+
+    if sort == 'price_low':
+        products = products.order_by('price')
+
+    elif sort == 'price_high':
+        products = products.order_by('-price')
+
+    elif sort == 'name_asc':
+        products = products.order_by('name')
+
+    elif sort == 'name_desc':
+        products = products.order_by('-name')
+
     return render(
         request,
         'products/product_list.html',
         {
             'products': products,
             'query': query,
+            'sort': sort,
         }
     )
+def home(request):
+    featured_products = Product.objects.all()[:4]
+
+    return render(
+        request,
+        'products/home.html',
+        {
+            'featured_products': featured_products
+        }
+    )   
